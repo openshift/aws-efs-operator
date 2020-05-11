cfba05dc2d0075d417aebde1f5f0930057b1420e
Eric Fried <efried@redhat.com>
2020-05-11 14:19:46 -0500

    Update deleteStatics.sh for dynamic namespaces
    Forgot to update the cleanup helper script for dynamic namespaces.

--- hack/deleteStatics.sh
+++ hack/deleteStatics.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-oc delete -n openshift-efs-csi \
+oc delete \
   scc/efs-csi-scc \
   csidriver/efs.csi.aws.com \
   storageclass/efs-sc \
   serviceaccount/efs-csi-sa \
-  daemonset/efs-csi-node \
-  namespace/openshift-efs-csi
+  daemonset/efs-csi-node
